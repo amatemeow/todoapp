dde77d2d14e4f03935c0688b7e9a3e8b53ddd330
amaterasuzxc <igarjar@gmail.com>
2022-12-01 13:24:30 +0300

    Unused removed

--- src/main/kotlin/app/todo/CurrentTimezone.kt
+++
@@ -1,5 +0,0 @@
-package app.todo
-
-@Target(AnnotationTarget.FIELD, AnnotationTarget.LOCAL_VARIABLE,
-    AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
-annotation class CurrentTimezone()
